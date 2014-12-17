724ee423e781fd2dbe50a5db90ee88a4fd354a54
Caleb Durenberger <caleb.durenberger@moody.edu>
2014-12-17 17:07:52 -0600

    changed colors

--- js/2x4.js
+++ js/2x4.js
@@ -1,9 +1,3 @@
-/*!
- * Start Bootstrap - 2x4 Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
@@ -29,7 +23,7 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
